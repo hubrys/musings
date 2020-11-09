4acc04bfcf5a84b51889972b7879a28554ba7764
Zachary Smith <zsmith0@gmail.com>
2020-11-08 19:11:07 -0500

    multiple boids

--- boids/src/boids.rs
+++ boids/src/boids.rs
@@ -8,6 +8,7 @@ use amethyst::core::math::Vector3;
 use crate::components::Boid;
 use crate::config::FlockConfig;
 use amethyst::utils::application_root_dir;
+use crate::utils::deg_to_rad;
 
 pub const WINDOW_WIDTH: f32 = 1200.0;
 pub const WINDOW_HEIGHT: f32 = 800.0;
@@ -18,12 +19,22 @@ pub struct Flock {}
 impl SimpleState for Flock {
   fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
     let world = data.world;
-    let arena_size = {
+    let (arena_size, boid_count) = {
       let config = world.read_resource::<FlockConfig>();
-      config.arena_size
+      (config.arena_size, config.boid_count)
     };
 
-    create_boid(world);
+    let sprite_sheet = load_sprite_sheet(world);
+    let sprite_render = SpriteRender::new(sprite_sheet, 0);
+    for i in 0..boid_count {
+      let pct = i as f32 / boid_count as f32;
+      create_boid(
+        world,
+        sprite_render.clone(),
+        [arena_size[0] * pct, arena_size[1] * pct],
+        360.0 * pct
+      )
+    }
     init_camera(world, arena_size);
   }
 
@@ -56,15 +67,18 @@ fn init_camera(world: &mut World, arena_size: [f32; 2]) {
     .build();
 }
 
-fn create_boid(world: &mut World) {
+fn create_boid(
+  world: &mut World,
+  sprite: SpriteRender,
+  location: [f32; 2],
+  rotation: f32) {
   let mut transform = Transform::default();
-  transform.set_translation_xyz(0.0, 0.0, 0.0);
+  transform.set_translation_xyz(location[0], location[1], 0.0);
+  transform.set_rotation_2d(deg_to_rad(rotation));
   transform.set_scale(Vector3::new(0.1, 0.1, 1.0));
-  let sprite_sheet = load_sprite_sheet(world);
-  let sprite_render = SpriteRender::new(sprite_sheet, 0);
   world
     .create_entity()
-    .with(sprite_render)
+    .with(sprite)
     .with(transform)
     .with(Boid::default())
     .build();
--- boids/src/config.rs
+++ boids/src/config.rs
@@ -3,6 +3,8 @@ use serde::{Serialize, Deserialize};
 #[derive(Debug, Deserialize, Serialize)]
 pub struct FlockConfig {
   pub arena_size: [f32; 2],
+  /// Number of boids to create
+  pub boid_count: u32,
   /// Boid speed in units per second
   pub boid_speed: f32,
   /// Turn speed in degrees per second
@@ -13,6 +15,7 @@ impl Default for FlockConfig {
   fn default() -> Self {
     FlockConfig {
       arena_size: [1200.0, 800.0],
+      boid_count: 5,
       boid_speed: 100.0,
       turning_speed: 180.0,
     }
